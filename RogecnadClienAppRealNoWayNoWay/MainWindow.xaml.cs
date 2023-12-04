4bdcef2b4fdc5258b2c1800ea03fc1b4a732b2e4
NotSpir <micheilkubrakin@gmail.com>
2023-12-04 19:10:35 +0300

    Создан интерфейс главного окна

--- RogecnadClienAppRealNoWayNoWay/MainWindow.xaml.cs
+++ RogecnadClienAppRealNoWayNoWay/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace RogecnadClienAppRealNoWayNoWay
 {
@@ -20,9 +21,60 @@ namespace RogecnadClienAppRealNoWayNoWay
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer;
+
+        double panelWidth;
+        bool hidden;
         public MainWindow()
         {
             InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 10);
+            timer.Tick += Timer_Tick;
+
+
+            panelWidth = sidePanel.Width;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+
+            if (hidden)
+            {
+                sidePanel.Width += 2;
+                if (sidePanel.Width >= panelWidth)
+                {
+                    timer.Stop();
+                    ExpandShortBtn.IsEnabled = true;
+                    IconExpandImage.Source = new BitmapImage(new Uri(@"/Resources/CircledArrowLeft.png", UriKind.Relative));
+                    hidden = false;
+                }
+            }
+            else
+            {
+                sidePanel.Width -= 2;
+                if (sidePanel.Width <= 100)
+                {
+                    timer.Stop();
+                    ExpandShortBtn.IsEnabled = true;
+                    IconExpandImage.Source = new BitmapImage(new Uri(@"/Resources/CircledArrowRight.png", UriKind.Relative));
+                    hidden = true;
+                }
+            }
+        }
+
+
+        private void PanelHeader_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void ExpandShortBtn_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
         }
     }
 }
