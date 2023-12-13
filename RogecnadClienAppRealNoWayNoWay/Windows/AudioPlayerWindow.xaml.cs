bd009f6f116012fa1471bef324a767fd07478248
NotSpir <micheilkubrakin@gmail.com>
2023-12-13 20:17:27 +0300

    Завершен UI аудиопроигрывателя

--- RogecnadClienAppRealNoWayNoWay/Windows/AudioPlayerWindow.xaml.cs
+++ RogecnadClienAppRealNoWayNoWay/Windows/AudioPlayerWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace RogecnadClienAppRealNoWayNoWay.Windows
 {
@@ -29,14 +30,19 @@ namespace RogecnadClienAppRealNoWayNoWay.Windows
         TimeSpan currentTime = new TimeSpan(0);
         bool IsPlaying = false;
         string currentTrackID = "";
+        DispatcherTimer timer;
         public AudioPlayerWindow()
         {
             InitializeComponent();
+
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 10);
+            timer.Tick += Timer_Tick;
         }
 
         private void audioSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-
+            AudioPlayer.soundPlayer.Position = TimeSpan.FromSeconds(audioSlider.Value);
         }
 
         private void ChannelBtn_Click(object sender, RoutedEventArgs e)
@@ -60,12 +66,14 @@ namespace RogecnadClienAppRealNoWayNoWay.Windows
                 currentTime = AudioPlayer.StripMilliseconds(AudioPlayer.soundPlayer.Position);
                 timeDisplayTextBlock.Text = $"{currentTime}/{duration}";
                 AudioPlayer.Stop(AudioPlayer.soundPlayer);
+                timer.Stop();
             }
             else
             {
                 IsPlaying = true;
                 imagePlayButton.Source = new BitmapImage(new Uri(@"/Resources/StopButton.png", UriKind.Relative));
                 AudioPlayer.Play(AudioPlayer.soundPlayer, currentTime);
+                timer.Start();
             }
         }
 
@@ -127,8 +135,21 @@ namespace RogecnadClienAppRealNoWayNoWay.Windows
                 return;
             }
             AppManager.audioWindow.Show();
+            IsPlaying = true;
+            var result = FirebaseClientModel.client.Get("Soundtracks\\" + trackID);
+            if (TimeSpan.TryParse(result.ResultAs<SoundTrack>().Duration, out duration))
+            {
+                timeDisplayTextBlock.Text = $"{currentTime}/{duration}";
+            }
+            songnameTextBlock.Text = result.ResultAs<SoundTrack>().TrackName;
+            channelNameTB.Text = result.ResultAs<SoundTrack>().getUserName;
+            imagePlayButton.Source = new BitmapImage(new Uri(@"/Resources/StopButton.png", UriKind.Relative));
+            audioSlider.Maximum = duration.TotalSeconds;
+            audioSlider.Minimum = 0;
+            audioSlider.Value = 0;
             AudioPlayer.soundPlayer = AudioPlayer.InitializePlayer(fileName);
             AudioPlayer.Play(AudioPlayer.soundPlayer, new TimeSpan(0));
+            timer.Start();
         }
 
         private void OnOpened(object sender, RoutedEventArgs e)
@@ -139,6 +160,49 @@ namespace RogecnadClienAppRealNoWayNoWay.Windows
         private void OnClosed(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            AudioPlayer.Stop(AudioPlayer.soundPlayer);
+        }
+
+        private void Window_Deactivated(object sender, EventArgs e)
+        {
+            AppManager.audioWindow.Topmost = true;
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var desktopWorkingArea = System.Windows.SystemParameters.WorkArea;
+            this.Left = desktopWorkingArea.Right - this.Width;
+            this.Top = desktopWorkingArea.Bottom - this.Height;
+        }
+
+        
+        public void RenderSlider()
+        {
+            timer.Start();
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+
+            if (IsPlaying)
+            {
+                audioSlider.Value = AudioPlayer.soundPlayer.Position.TotalSeconds;
+                currentTime = AudioPlayer.StripMilliseconds(AudioPlayer.soundPlayer.Position);
+                timeDisplayTextBlock.Text = $"{currentTime}/{duration}";
+                if (audioSlider.Value >= duration.TotalSeconds)
+                {
+                    AudioPlayer.soundPlayer.Position = new TimeSpan(0);
+                }
+            }
         }
     }
 }
